114171821c0ef0a111d5d77fad666f1950167200
Ali Daley <ali.daley72@gmail.com>
2021-11-08 15:13:17 -0500

    exports setup for atm

---
+++ atm.js
@@ -0,0 +1,23 @@
+"use strict"
+
+function getBalance(){
+    break;
+}
+
+function withdraw(){
+    break;
+}
+
+function deposit(){
+    break;
+}
+
+function validatePin(){
+    break;
+}
+
+//exports
+module.exports.getBalance = getBalance;
+module.exports.withdraw = withdraw;
+module.exports.deposit = deposit;
+module.exports.validatePin = validatePin;
